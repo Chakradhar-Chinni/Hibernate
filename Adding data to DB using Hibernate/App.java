a25ac09de25e4a2be6cbb13f1aedc972a70b2b0d
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-06-13 22:39:47 +0530

    Create App.java

---
+++ Adding data to DB using Hibernate/App.java
@@ -0,0 +1,37 @@
+package com.telusko.DemoHib;  
+
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory; 
+import org.hibernate.Transaction;                 
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistryBuilder; 
+
+public class App  
+{
+    public static void main( String[] args )
+    {
+        //System.out.println( "Hello World!" );	
+    	Alien telusko = new Alien(); 
+    	telusko.setAid(103); 
+    	telusko.setAcolor("Green");
+    	telusko.setAname("Arya");
+        
+        Configuration con = new Configuration().configure().addAnnotatedClass(Alien.class);
+        ServiceRegistry reg = new ServiceRegistryBuilder().applySettings(con.getProperties()).buildServiceRegistry();
+        
+        SessionFactory sf = con.buildSessionFactory(); 
+        Session session = sf.openSession();
+        
+        Transaction tx = session.beginTransaction();
+        
+               		
+        session.save(telusko);
+        
+        tx.commit();
+        
+   }
+}
