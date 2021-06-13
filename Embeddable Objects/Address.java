a04a0f4e4712bd9889e0c930b3327268a3ca0297
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-06-13 23:40:13 +0530

    Create Address.java
    Embedded File

---
+++ Embeddable Objects/Address.java
@@ -0,0 +1,35 @@
+package com.telusko.DemoHib;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Address 
+{
+	private String home;
+	private String office; 
+	private String residential;
+	public String getHome() {
+		return home;
+	}
+	public void setHome(String home) {
+		this.home = home;
+	}
+	public String getOffice() {
+		return office;
+	}
+	public void setOffice(String office) {
+		this.office = office;
+	}
+	public String getResidential() {
+		return residential;
+	}
+	public void setResidential(String residential) {
+		this.residential = residential;
+	}
+	@Override
+	public String toString() {
+		return "Address [home=" + home + ", office=" + office + ", residential=" + residential + "]";
+	}
+	 
+	 
+}
