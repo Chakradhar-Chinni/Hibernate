318efd7c3967327da8447758e1a5b5a225273593
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-06-16 12:52:59 +0530

    Create App.java

---
+++ Level 2 Caching/App.java
@@ -0,0 +1,40 @@
+package com.telusko.relations;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+ 
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory; 
+import org.hibernate.Transaction;                  
+import org.hibernate.cfg.Configuration;  
+public class App 
+{
+	public static void main(String[] args)
+	{
+		Student s = null;
+		
+		    Configuration con = new Configuration().configure().addAnnotatedClass(Student.class).addAnnotatedClass(Laptop.class);
+        ServiceRegistry reg = new ServiceRegistryBuilder().applySettings(con.getProperties()).buildServiceRegistry();
+        SessionFactory sf = con.buildSessionFactory();
+        
+        Session session1 = sf.openSession();
+        
+        session1.beginTransaction();
+        	s = (Student) session1.get(Student.class,20);  //line 1
+        	//.get returns object method so we type cast it to class name
+        	System.out.println(s);
+       	session1.getTransaction().commit();
+       	session1.close();
+       	
+       	                 
+     	 Session session2 = sf.openSession();
+	     session2.beginTransaction();
+	        	s = (Student) session2.get(Student.class,20);   //line 2
+	        	System.out.println(s);
+	     session2.getTransaction().commit();
+	     session2.close();
+        	
+        
+	}
+}
