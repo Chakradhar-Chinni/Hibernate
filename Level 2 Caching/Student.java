784c4ee72e0f4949a7d55a1fc195b108b2605179
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-06-16 13:15:03 +0530

    Create Student.java

---
+++ Level 2 Caching/Student.java
@@ -0,0 +1,48 @@
+package com.telusko.relations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Cacheable;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.cfg.Configuration;
+@Entity
+@Cacheable
+@Cache(usage = CacheConcurrencyStrategy.READ_ONLY)
+public class Student 
+{
+	@Id
+	private int Rollno;
+	private String Name;
+	private int Marks;
+
+	public int getRollno() {
+		return Rollno;
+	}
+	public void setRollno(int rollno) {
+		Rollno = rollno;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public int getMarks() {
+		return Marks;
+	}
+	public void setMarks(int marks) {
+		Marks = marks;
+	}
+	@Override
+	public String toString() {
+		return "Student [Rollno=" + Rollno + ", Name=" + Name + ", Marks=" + Marks + "]";
+	}
+	
+
+}
